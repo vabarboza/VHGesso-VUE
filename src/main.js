48256f365ddbefe960367dd2542987a365ef1e85
Vinicius Araujo <56658014+vabarboza@users.noreply.github.com>
2021-09-21 21:33:40 -0300

    Toast

--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import JQuery from "jquery";
+window.$ = JQuery
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTXGxuk4vPzuM_XxeRI-637JPPxCaSZzk",
-  authDomain: "vhgesso-2dc73.firebaseapp.com",
-  projectId: "vhgesso-2dc73",
-  storageBucket: "vhgesso-2dc73.appspot.com",
-  messagingSenderId: "291800489169",
-  appId: "1:291800489169:web:db21238d272b693bfcb9f0",
-  measurementId: "G-V3YG04F6QD"
+    apiKey: "AIzaSyCTXGxuk4vPzuM_XxeRI-637JPPxCaSZzk",
+    authDomain: "vhgesso-2dc73.firebaseapp.com",
+    projectId: "vhgesso-2dc73",
+    storageBucket: "vhgesso-2dc73.appspot.com",
+    messagingSenderId: "291800489169",
+    appId: "1:291800489169:web:db21238d272b693bfcb9f0",
+    measurementId: "G-V3YG04F6QD",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-
     <div class="container-fluid mt-3 mb-3">
+      <a
+        class="btn btn-success"
+        onclick="toastr.success('Hi! I am success message.');"
+        >Success message</a
+      >
 
       <div class="container-sm my-5">
         <div class="card border border-dark rounded-sm bg-white">
@@ -12,14 +16,13 @@
 
         <!-- Section: Block Content -->
         <section class="dark-grey-text text-center my-5">
-
           <p class="text-center text-muted w-responsive mx-auto mb-5">
-            Estes são alguns exemplos de nossos serviços e produtos nas quais temos para oferecer.
+            Estes são alguns exemplos de nossos serviços e produtos nas quais
+            temos para oferecer.
           </p>
 
           <!-- Grid row -->
           <div class="row">
-
             <!-- Grid column -->
             <div class="col-md-4 mb-4 wow fadeInUp">
               <!-- Card Wider -->
@@ -27,10 +30,10 @@
                 <!-- Card image -->
                 <div class="view view-cascade overlay zoom">
                   <img
-                    class="card-img-top "
+                    class="card-img-top"
                     src="img/content/inicio/01.jpg"
                     alt="Card image cap"
-                  >
+                  />
                   <a href="{{ route('forro-liso') }}">
                     <div class="mask rgba-white-slight"></div>
                   </a>
@@ -41,27 +44,27 @@
                   <h4 class="card-title"><strong>Forro Liso</strong></h4>
                   <!-- Text -->
                   <p class="card-text">
-                    É a tendência do momento usado para revestir paredes e teto pela sua versatilidade,
-                    plasticidade e acabamento. Ele traz um toque sutil e fino, paredes com bom acabamento de
-                    Gesso Liso e uma boa tinta é notória sua diferença e classe. </p>
+                    É a tendência do momento usado para revestir paredes e teto
+                    pela sua versatilidade, plasticidade e acabamento. Ele traz
+                    um toque sutil e fino, paredes com bom acabamento de Gesso
+                    Liso e uma boa tinta é notória sua diferença e classe.
+                  </p>
                 </div>
               </div>
               <!-- Card Wider -->
-
             </div>
             <!-- Grid column -->
 
             <div class="col-md-4 mb-4 wow fadeInUp">
-
               <!-- Card Wider -->
               <div class="card card-cascade wider">
                 <!-- Card image -->
                 <div class="view view-cascade overlay zoom">
                   <img
-                    class="card-img-top "
+                    class="card-img-top"
                     src="img/content/inicio/02.jpg"
                     alt="Card image cap"
-                  >
+                  />
                   <a href="{{ route('molduras') }}">
                     <div class="mask rgba-white-slight"></div>
                   </a>
@@ -72,29 +75,30 @@
                   <h4 class="card-title"><strong>Molduras</strong></h4>
                   <!-- Text -->
                   <p class="card-text">
-                    Molduras de Gesso há tempos é o toque final de acabamento colocando requinte nos
-                    perímetros dos ambientes, nós da VH Gesso utilizamos molduras fabricadas com materiais
-                    de qualidade e disponibilizamos mão de obra qualificada para a aplicação da mesma, além
-                    de ter opções à pronta entrega e modelos atuais com estilos contemporâneos de
-                    arquitetura moderna. </p>
+                    Molduras de Gesso há tempos é o toque final de acabamento
+                    colocando requinte nos perímetros dos ambientes, nós da VH
+                    Gesso utilizamos molduras fabricadas com materiais de
+                    qualidade e disponibilizamos mão de obra qualificada para a
+                    aplicação da mesma, além de ter opções à pronta entrega e
+                    modelos atuais com estilos contemporâneos de arquitetura
+                    moderna.
+                  </p>
                 </div>
               </div>
               <!-- Card Wider -->
-
             </div>
             <!-- Grid column -->
 
             <div class="col-md-4 mb-4 wow fadeInUp">
-
               <!-- Card Wider -->
               <div class="card card-cascade wider">
                 <!-- Card image -->
                 <div class="view view-cascade overlay zoom">
                   <img
-                    class="card-img-top "
+                    class="card-img-top"
                     src="img/content/inicio/03.jpg"
                     alt="Card image cap"
-                  >
+                  />
                   <a href="{{ route('gesso-acartonado') }}">
                     <div class="mask rgba-white-slight"></div>
                   </a>
@@ -105,23 +109,22 @@
                   <h4 class="card-title"><strong>Gesso Acartonado</strong></h4>
                   <!-- Text -->
                   <p class="card-text">
-                    Esse sistema é popularmente chamado de parede ou forro de drywall, por isso, muitas
-                    pessoas confundem forro drywall e gesso acartonado, achando que são produtos diferentes,
-                    quando na verdade o drywall é o sistema de construção utilizado para a instalação do
-                    gesso acartonado. </p>
+                    Esse sistema é popularmente chamado de parede ou forro de
+                    drywall, por isso, muitas pessoas confundem forro drywall e
+                    gesso acartonado, achando que são produtos diferentes,
+                    quando na verdade o drywall é o sistema de construção
+                    utilizado para a instalação do gesso acartonado.
+                  </p>
                 </div>
               </div>
               <!-- Card Wider -->
-
             </div>
             <!-- Grid column -->
-
           </div>
           <!-- Grid row -->
 
           <!-- Grid row -->
           <div class="row">
-
             <!-- Grid column -->
             <div class="col-md-4 mb-4 wow fadeInUp">
               <!-- Card Wider -->
@@ -129,10 +132,10 @@
                 <!-- Card image -->
                 <div class="view view-cascade overlay zoom">
                   <img
-                    class="card-img-top "
+                    class="card-img-top"
                     src="img/content/inicio/04.jpg"
                     alt="Card image cap"
-                  >
+                  />
                   <a href="{{ route('sancas') }}">
                     <div class="mask rgba-white-slight"></div>
                   </a>
@@ -143,29 +146,29 @@
                   <h4 class="card-title"><strong>Sancas</strong></h4>
                   <!-- Text -->
                   <p class="card-text">
-                    Sanca ou rebaixamento de teto realmente fazem a diferença nos mais diferentes ambientes
-                    trazendo estilo, requinte e beleza. A VH Gesso tem sempre o melhor projeto de sanca e
-                    iluminação com as melhores soluções. Sempre atendendo o Perfil e necessidade de nossos
-                    Clientes com projetos únicos e exclusivos.
+                    Sanca ou rebaixamento de teto realmente fazem a diferença
+                    nos mais diferentes ambientes trazendo estilo, requinte e
+                    beleza. A VH Gesso tem sempre o melhor projeto de sanca e
+                    iluminação com as melhores soluções. Sempre atendendo o
+                    Perfil e necessidade de nossos Clientes com projetos únicos
+                    e exclusivos.
                   </p>
                 </div>
               </div>
               <!-- Card Wider -->
-
             </div>
             <!-- Grid column -->
 
             <div class="col-md-4 mb-4 wow fadeInUp">
-
               <!-- Card Wider -->
               <div class="card card-cascade wider">
                 <!-- Card image -->
                 <div class="view view-cascade overlay zoom">
                   <img
-                    class="card-img-top "
+                    class="card-img-top"
                     src="img/content/inicio/05.jpg"
                     alt="Card image cap"
-                  >
+                  />
                   <a href="{{ route('colunas') }}">
                     <div class="mask rgba-white-slight"></div>
                   </a>
@@ -173,33 +176,35 @@
                 <!-- Card content -->
                 <div class="card-body card-body-cascade text-center">
                   <!-- Title -->
-                  <h4 class="card-title"><strong>Colunas Decorativas</strong></h4>
+                  <h4 class="card-title">
+                    <strong>Colunas Decorativas</strong>
+                  </h4>
                   <!-- Text -->
                   <p class="card-text">
-                    As colunas são utilizadas na decoração do ambiente e são, também, muito utilizadas para
-                    a limitação (divisão) do ambiente. Elas podem ser lisas ou caneladas "tipo grego". Tanto
-                    a coluna lisa como a coluna decorada têm acompanhamento da base, para acabamento no pé
-                    da coluna, e do capitel, para acompanhamento em cima da coluna. As Colunas em gesso são
-                    muito úteis na decoração externa.
+                    As colunas são utilizadas na decoração do ambiente e são,
+                    também, muito utilizadas para a limitação (divisão) do
+                    ambiente. Elas podem ser lisas ou caneladas "tipo grego".
+                    Tanto a coluna lisa como a coluna decorada têm
+                    acompanhamento da base, para acabamento no pé da coluna, e
+                    do capitel, para acompanhamento em cima da coluna. As
+                    Colunas em gesso são muito úteis na decoração externa.
                   </p>
                 </div>
               </div>
               <!-- Card Wider -->
-
             </div>
             <!-- Grid column -->
 
             <div class="col-md-4 mb-4 wow fadeInUp">
-
               <!-- Card Wider -->
               <div class="card card-cascade wider">
                 <!-- Card image -->
                 <div class="view view-cascade overlay zoom">
                   <img
-                    class="card-img-top "
+                    class="card-img-top"
                     src="img/content/inicio/06.jpg"
                     alt="Card image cap"
-                  >
+                  />
                   <a href="{{ route('gesso-decorado') }}">
                     <div class="mask rgba-white-slight"></div>
                   </a>
@@ -210,10 +215,12 @@
                   <h4 class="card-title"><strong>Gesso Decorado</strong></h4>
                   <!-- Text -->
                   <p class="card-text">
-                    Esse revestimento é feito através de placas de gesso com desenhos em auto relevo em uma
-                    das faces, que cria um efeito sensacional e maravilhoso de luz e sombra nas paredes que
-                    são aplicados. As texturas e composições que esse revestimento agrega no ambiente
-                    valoriza e aumenta muito a sensação de requinte e conforto do espaço.
+                    Esse revestimento é feito através de placas de gesso com
+                    desenhos em auto relevo em uma das faces, que cria um efeito
+                    sensacional e maravilhoso de luz e sombra nas paredes que
+                    são aplicados. As texturas e composições que esse
+                    revestimento agrega no ambiente valoriza e aumenta muito a
+                    sensação de requinte e conforto do espaço.
                   </p>
                 </div>
               </div>
@@ -226,8 +233,26 @@
         <!-- Section: Block Content -->
       </div>
     </div>
-
   </div>
 </template>
+
+
+//Link do site onde esta o elemento
+//https://mdbootstrap.com/docs/b4/jquery/javascript/notifications/#introduction
+<script>
+export default {
+  methods: {
+    notifi() {
+     // $('#element').toast('show')
+    },
+    alert() {
+     // toastr.info('Hi! I am info message.');
+    }
+  },
+  mounted() {
+    this.notifi()
+  }
+}
+</script>
 
 
